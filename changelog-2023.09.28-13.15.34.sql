fd2ff2393edb8c169237020ed298d80d594062db
santhosh-icanio <134063057+santhosh-icanio@users.noreply.github.com>
2023-09-28 17:19:50 +0530

    Update changelog-2023.09.28-13.15.34.sql

--- changelog-2023.09.28-13.15.34.sql
+++ changelog-2023.09.28-13.15.34.sql
@@ -5,3 +5,8 @@ INSERT INTO test (ID, Name) VALUES (3, 'Value1');
 INSERT INTO test (ID, Name) VALUES (4, 'Value2');
 
 --rollback DELETE FROM test WHERE ID IN (3, 4);
+--changeset nvoxland:2
+INSERT INTO test (ID, Name) VALUES (5, 'kishor');
+INSERT INTO test (ID, Name) VALUES (6, 'santhosh');
+
+--rollback DELETE FROM test WHERE ID IN (5, 6);
